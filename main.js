81faa7260d5c25ca4a0ef11e6e63032b8aee8e5f
Tklao <carlosmelo1295@gmail.com>
2020-10-16 15:12:45 -0300

    new session⚡️

--- main.js
+++ main.js
@@ -1,86 +1,90 @@
+"use strict";
 const $btnStart = document.querySelector("#btnStart");
 const $btnStop = document.querySelector("#btnStop");
-
-//----------------------------------------------
-
 const timePomodoro = document.querySelector("#time");
-
-//----------------------------------------------
-
 const audio = document.querySelector("#audio");
 const pressBtn = document.querySelector("#press");
-
-//----------------------------------------------
+const tomataoAudio = document.querySelector("#tomataoAudio");
 
 //ouvinte do evento
 $btnStart.addEventListener("click", startCount);
 $btnStop.addEventListener("click", stopCount);
 
-//----------------------------------------------
-
 let interval = null;
 let minutes = "24";
 let seconds = "59";
 
-//----------------------------------------------
-
 let size = 2;
 
-//----------------------------------------------
-
 function startCount() {
-  pressBtn.play();
-  interval = setInterval(() => {
-    timePomodoro.innerHTML = `${minutes}:${seconds}`;
-    seconds--;
-    if (seconds == -1) {
-      minutes--;
-      seconds = 59;
-	}
-	else if (seconds < 10) {
-      seconds = seconds.toString();
-      while (seconds.length < size) {
-        seconds = "0" + seconds;
-      }
-	}
-	else if (minutes < 10 && minutes > -1) {
-      minutes = minutes.toString();
-      while (minutes.length < size) {
-        minutes = "0" + minutes;
-      }
-	}
-	else if (minutes == -1) {
-      clearInterval(interval);
-      audio.play();
-      timePomodoro.innerHTML = `Pausa para o ☕`;
-      minutes = "24";
-      seconds = "59";
-      img.style.rotate = "360deg";
-    }
-  }, 1000);
+   "use strict";
+   pressBtn.play();
+   interval = setInterval(() => {
+      timePomodoro.innerHTML = `${minutes}:${seconds}`;
+      seconds--;
 
-  img.style.width = "200px";
-  img.style.transition = "500ms";
+      if (seconds == -1) {
+         minutes--;
+         seconds = 59;
+      }
+      if (seconds < 10) {
+         seconds = seconds.toString();
+         while (seconds.length < size) {
+            seconds = "0" + seconds;
+         }
+      }
+      if (minutes < 10 && minutes > -1) {
+         minutes = minutes.toString();
+         while (minutes.length < size) {
+            minutes = "0" + minutes;
+         }
+      }
+      if (minutes == -1) {
+         clearInterval(interval);
+         $btnStart.disabled = false;
+         audio.play();
+         timePomodoro.innerHTML = `Pausa para o ☕`;
+         Swal.fire(`Etapa concluída!`);
+         minutes = "24";
+         seconds = "59";
+         img.style.rotate = "360deg";
+      }
+   }, 10);
+
+   img.style.width = "200px";
+   img.style.transition = "500ms";
+
+   $btnStart.disabled = true; // Desabilitar botão após o click
 }
 
-//criar uma contagem decremente de 5 min para a pausa
+//criar uma contagem decrescente de 5 min para a pausa
 
 //----------------------------------------------
 
 function stopCount() {
-  clearInterval(interval);
-  pressBtn.play();
-  img.style.width = "100px";
+   "use strict";
+   clearInterval(interval);
+   pressBtn.play();
+   img.style.width = "100px";
+
+   $btnStart.disabled = false; //Habilitar "start" ao clicar em "stop"
 }
 
 //----------------------------------------------
 
 let img = document.createElement("img");
 document.querySelector(".containerTime").appendChild(img);
-const tomataoAudio = document.querySelector("#tomataoAudio");
 
 setTimeout(() => {
-  tomataoAudio.play();
-  img.src = "24692--ingredient_detail_ingredient-2.png";
-  img.style.width = "100px";
+   tomataoAudio.play();
+   img.src = "24692--ingredient_detail_ingredient-2.png";
+   img.title = "Eu sou o tomatão e vou te ajudar na hora dos estudos!"
+   img.style.width = "100px";
+   img.style.cursor = "pointer"
 }, 1000);
+
+
+const arrow_down = document.querySelector("#arrow-down").addEventListener('click', () => {
+   scrollTo(0, window.innerHeight)
+
+})
