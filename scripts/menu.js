64b146015812c062f4584668874e30f41f03b015
Tklao <carlosmelo1295@gmail.com>
2020-11-06 15:12:19 -0300

    :rocket: button effect on press

--- scripts/menu.js
+++ scripts/menu.js
@@ -4,7 +4,10 @@ $menuBtn.addEventListener("click", () => {
     const menu = document.querySelector("#menu").style.display;
     if (menu != "block") {
         document.querySelector("#menu").style.display = "block";
+        $menuBtn.style.scale = "0.90"
+
     } else {
         document.querySelector("#menu").style.display = "none";
+        $menuBtn.style.scale = "1.1"
     }
 });
